02956a96278655aa47bbf8a60ed73e694e7f6faa
kakeru13468 <auchergod@gmail.com>
2024-10-21 13:07:19 +0800

    [feat]update ui anime

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -107,8 +107,9 @@ const Home = () => {
                   className="bg-white p-6 rounded-lg shadow-lg cursor-pointer"
                   initial={{ opacity: 0, y: 20 }} 
                   animate={{ opacity: 1, y: 0 }} 
-                  transition={{ duration: 0.5, delay: index * 0.2 }} 
-                  whileHover={{ scale: 1.1 }} 
+                  transition={{ duration: 0.5}} 
+                  whileHover={{ scale: 1.1,transition: { duration: 0.2 ,type: "spring", stiffness: 400, damping: 10 }, }} 
+                  whileTap={{ scale: 0.9 }}
                   onClick={() => navigate('/LyricsStudy', { state: { songData: song } })}
                 >
                   <h3 className="text-xl font-semibold mb-4">{song.title}</h3>
